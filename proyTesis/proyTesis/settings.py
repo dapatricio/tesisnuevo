49c3a26e118494ddc813c6be3ad5d51c1a9972b7
Daniel Peñarreta <dapatricio9405@gmail.com>
2020-10-19 02:23:55 -0500

    actualizacion pagina web final

--- proyTesis/proyTesis/settings.py
+++ proyTesis/proyTesis/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = [
     "172.17.22.161",
     "192.168.100.16",
     "192.168.100.2",
+    "161.35.51.253",
 ]
 
 # Application definition
@@ -94,29 +95,29 @@ WSGI_APPLICATION = "proyTesis.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.mysql",
-#        "NAME": "webencuestas1",
-#        "USER": "root",
-#        "PASSWORD": "",
-#        "HOST": "localhost",
-#        "PORT": "3306",
-#    }
-#}
-#
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "webencuestas1",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
+"""
 #Base de datos postgres server
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "competenciasdigitales",
         "USER": "adminsite",
-        "PASSWORD": "DaPatr1c1o",
+        "PASSWORD": "DaPatr1c10.1994",
         "HOST": "localhost",
         "PORT": "5432",
     }
 }
-
+"""
 
 # DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "mydatabase"}}
 # Password validation
@@ -151,9 +152,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 # PROJECT_DIR = os.path.dirname(os.path.abspath(_file_))
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
