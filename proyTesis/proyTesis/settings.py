90d7ed408dc922e8f74d1bc264056c1d6759e745
Daniel Peñarreta <dapatricio9405@gmail.com>
2021-06-17 16:33:10 -0500

    Actualizacion url

--- proyTesis/proyTesis/settings.py
+++ proyTesis/proyTesis/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = "proyTesis.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
@@ -120,7 +120,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-"""
 
 # DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "mydatabase"}}
 # Password validation
